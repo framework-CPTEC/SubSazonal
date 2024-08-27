6610f6ef1c7c46075dd5322333cd1bc87a927182
Python - Distribuição de Modelos Numéricos <122042628+framework-CPTEC@users.noreply.github.com>
2024-08-27 14:38:26 -0300

    Create subsazonal.py

---
+++ subsazonal.py
@@ -0,0 +1,11 @@
+
+"""
+SubSazonal - Biblioteca Python para a distibuição de Dados dos Modelos Numéricos.
+"""
+
+__version__ = "0.1.0"
+
+
+class InvalidKindError(Exception):
+    """Raised if the kind is invalid."""
+    pass
